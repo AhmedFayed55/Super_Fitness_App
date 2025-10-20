95c9b711aae0d2d296bdbcf30f77f2d6c29d1367
Mostafa_Amer <161795892+MostafaAmer978@users.noreply.github.com>
2025-10-20 23:20:54 +0300

    refactor: add AppTextField.dart & update app_theme.dart and custom_elevated_button.dart

--- lib/core/components/custom_elevated_button.dart
+++ lib/core/components/custom_elevated_button.dart
@@ -31,24 +31,24 @@ class CustomElevatedButton extends StatelessWidget {
         curve: Curves.easeInOut,
         child: isLoading
             ? Container(
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: containerColor ?? context.colorScheme.primary,
-                  borderRadius: BorderRadius.circular(context.height * 0.1),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: containerColor ?? context.colorScheme.primary,
+            borderRadius: BorderRadius.circular(context.height * 0.1),
+          ),
+          child: SizedBox(
+            width: context.width * 0.08,
+            height: context.height * 0.03,
+            child: Theme(
+              data: ThemeData(
+                progressIndicatorTheme: ProgressIndicatorThemeData(
+                  color: loadingColor ?? context.colorScheme.onPrimary,
                 ),
-                child: SizedBox(
-                  width: context.width * 0.1,
-                  height: context.width * 0.1,
-                  child: Theme(
-                    data: ThemeData(
-                      progressIndicatorTheme: ProgressIndicatorThemeData(
-                        color: loadingColor ?? context.colorScheme.onPrimary,
-                      ),
-                    ),
-                    child: const CircularProgressIndicator(),
-                  ),
-                ),
-              )
+              ),
+              child: const CircularProgressIndicator(),
+            ),
+          ),
+        )
             : ElevatedButton(onPressed: onPressed, child: widget),
       ),
     );
