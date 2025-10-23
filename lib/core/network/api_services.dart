17d3dc1e73cd443477cbf28277c9d76833ea02dd
ahmed <anaa71137@gmail.com>
2025-10-24 02:31:07 +0300

    demo

--- lib/core/network/api_services.dart
+++ lib/core/network/api_services.dart
@@ -3,24 +3,7 @@ import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:super_fitness_app/core/network/network_constants.dart';
 import 'package:super_fitness_app/features/auth/login/data/models/response/login_response_dto.dart';
-
 import '../../features/auth/login/data/models/request/login_request_dto.dart';
-
-part 'api_services.g.dart';
-
-@RestApi()
-@injectable
-abstract class ApiServices {
-  @factoryMethod
-  factory ApiServices(Dio dio) = _ApiServices;
-
-  @POST(EndPoints.login)
-  Future<LoginResponseDto> login(@Body() LoginRequestDto loginRequest);
-}
-import 'package:dio/dio.dart';
-import 'package:injectable/injectable.dart';
-import 'package:retrofit/retrofit.dart';
-import 'package:super_fitness_app/core/network/network_constants.dart';
 import 'package:super_fitness_app/features/auth/forget_password/data_sources/models/request/forget_password_request_dto.dart';
 import 'package:super_fitness_app/features/auth/forget_password/data_sources/models/request/reset_password_request_dto.dart';
 import 'package:super_fitness_app/features/auth/forget_password/data_sources/models/request/verify_reset_code_request_dto.dart';
@@ -35,6 +18,9 @@ part 'api_services.g.dart';
 abstract class ApiServices {
   @factoryMethod
   factory ApiServices(Dio dio) = _ApiServices;
+
+  @POST(EndPoints.login)
+  Future<LoginResponseDto> login(@Body() LoginRequestDto loginRequest);
 
   @POST(EndPoints.forgotPassword)
   Future<ForgetPasswordResponseDto> forgotPassword(
--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,24 +1,17 @@
 abstract class AppAssets {
-  static const String homeIcon = 'assets/icons/home_icon.svg';
-  static const String profileIcon = 'assets/icons/profile_icon.svg';
-  static const String workoutIcon = 'assets/icons/workout_icon.svg';
-  static const String chatBotIcon = 'assets/icons/chat_bot_icon.svg';
-}
-abstract class AppAssets {
+  // Images
+  static const String authBackground = 'assets/images/auth_bg.png';
+  static const String loginBackground = "assets/images/login_background.png";
+  static const String appLogo = "assets/images/logo.png";
+  static const String fieldsBackground = "assets/images/fields_background.png";
   static const String onboardingMain = "assets/images/onboarding_1_bg.png";
   static const String onboarding1 = "assets/images/onboarding_1.png";
   static const String onboarding2 = "assets/images/onboarding_2.png";
   static const String onboarding3 = "assets/images/onboarding_3.png";
-}
-abstract class AppAssets {
-  static const String loginBackground = "assets/images/login_background.png";
-  static const String appLogo = "assets/images/logo.png";
-  static const String fieldsBackground = "assets/images/fields_background.png";
-}
-abstract class AppAssets {
-  // Images
-  static const String authBackground = 'assets/images/auth_bg.png';
-  static const String appLogo = 'assets/images/app-logo.png';
 
   // SVGs
+  static const String homeIcon = 'assets/icons/home_icon.svg';
+  static const String profileIcon = 'assets/icons/profile_icon.svg';
+  static const String workoutIcon = 'assets/icons/workout_icon.svg';
+  static const String chatBotIcon = 'assets/icons/chat_bot_icon.svg';
 }
