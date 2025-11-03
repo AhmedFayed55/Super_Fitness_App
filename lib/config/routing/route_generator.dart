91fd480859e01875b2288a07e9b8c936b523f9aa
Mostafa_Amer <161795892+MostafaAmer978@users.noreply.github.com>
2025-11-03 14:37:50 +0200

    feat(change_password): Finish ( Data & Domain ) Layers without Testing

--- lib/config/routing/route_generator.dart
+++ lib/config/routing/route_generator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:super_fitness_app/features/auth/change_password/presentation/pages/change_password_screen.dart';
 import 'package:super_fitness_app/features/auth/forget_password/presentation/pages/forget_password_screen.dart';
 import '../../features/auth/login/presentation/pages/login_screen.dart';
 import 'app_routes.dart';
@@ -11,6 +12,11 @@ class RouteGenerator {
       case AppRoutes.forgetPassword:
         return MaterialPageRoute(
           builder: (context) => const ForgetPasswordScreen(),
+        );
+
+      case AppRoutes.changePassword:
+        return MaterialPageRoute(
+          builder: (context) => const ChangePasswordScreen(),
         );
 
       default:
