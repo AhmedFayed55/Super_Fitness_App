af3f71407ef97679c55fb47ac4b295aa9eaa9049
Mostafa_Amer <161795892+MostafaAmer978@users.noreply.github.com>
2025-11-03 12:57:54 +0200

    feat(home): added Full Body Tab

--- lib/features/home_screen/domain/use_cases/upcoming_workouts/get_all_muscles_response_usecase.dart
+++ lib/features/home_screen/domain/use_cases/upcoming_workouts/get_all_muscles_response_usecase.dart
@@ -1,6 +1,7 @@
 import 'package:injectable/injectable.dart';
 import 'package:super_fitness_app/core/network/api_results.dart';
 import 'package:super_fitness_app/features/home_screen/domain/entities/upcoming_workouts/get_all_muscles_entity.dart';
+import 'package:super_fitness_app/features/home_screen/domain/entities/upcoming_workouts/muscles_group_dto_entity.dart';
 import 'package:super_fitness_app/features/home_screen/domain/repositories/home_repo.dart';
 
 @injectable
@@ -9,5 +10,21 @@ class GetAllMusclesResponseUseCase {
 
   GetAllMusclesResponseUseCase(this._repo);
 
-  Future<ApiResult<GetAllMusclesEntity>> call() => _repo.getAllMuscles();
+  Future<ApiResult<GetAllMusclesEntity>> call() async {
+    final result = await _repo.getAllMuscles();
+    if (result is ApiSuccessResult<GetAllMusclesEntity>) {
+      final originalList = result.data.musclesGroupDtoEntity;
+      final fullBodyTab = MusclesGroupDtoEntity(
+        id: "full_body",
+        name: "Full Body",
+      );
+      final updatedList = [fullBodyTab, ...originalList];
+      final updatedResponse = GetAllMusclesEntity(
+        message: result.data.message,
+        musclesGroupDtoEntity: updatedList,
+      );
+      return ApiSuccessResult<GetAllMusclesEntity>(data: updatedResponse);
+    }
+    return result;
+  }
 }
