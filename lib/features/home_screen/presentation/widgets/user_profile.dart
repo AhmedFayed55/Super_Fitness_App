89e383a8fa58a51f915ba1f1fd45641c29d650e6
Mostafa_Amer <amer2052001@gmail.com>
2025-11-11 15:26:10 +0200

    feat(home_screen): remove unused imports again

--- lib/features/home_screen/presentation/widgets/user_profile.dart
+++ lib/features/home_screen/presentation/widgets/user_profile.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 import 'package:super_fitness_app/config/theme/colors.dart';
 import 'package:super_fitness_app/core/extensions/extensions.dart';
-import 'package:super_fitness_app/features/home_screen/presentation/manager/home_view_model.dart';
 
 class UserProfile extends StatefulWidget {
   const UserProfile({super.key});
